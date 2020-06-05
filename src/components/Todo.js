678b34260cd61482311c54aa15f29c1d1194dfee
unknown <KalebJohnson45@yahoo.com>
2020-06-05 18:59:20 -0500

    Done

---
+++ src/components/Todo.js
@@ -0,0 +1,8 @@
+
+import React from 'react'
+
+export default props => 
+    <p
+        style={{textDecoration: props.todo.complete ? "line-through" : ''}} 
+        onClick={props.toggleComplete}>{props.todo.text}
+    </p>
